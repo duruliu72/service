ca26375111b4fa38df2446dde5dc4a21f394e89c
duruliu72 <duruliu72@gmail.com>
2018-06-07 20:08:51 +0600

    first commit 07062018

---
+++ js/script.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+    $('#offer-car1').owlCarousel({
+        loop: true,
+        margin: 25,
+        nav: false,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 4
+            }
+        }
+    });
+    $('#offer-car2').owlCarousel({
+        loop: true,
+        margin: 25,
+        nav: false,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 4
+            }
+        }
+    });
+    $('#offer-car3').owlCarousel({
+        loop: true,
+        margin: 25,
+        nav: false,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 4
+            }
+        }
+    });
+});
